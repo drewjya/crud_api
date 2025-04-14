22cf2f29439098a14e890b2d82f3eb3362709db1
Andre Wijaya <andre@Andres-Mac-mini.local>
2025-04-14 16:29:46 +0700

    Initial Commit

---
+++ src/main/kotlin/com/drewjya/crud_api/CrudApiApplication.kt
@@ -0,0 +1,11 @@
+package com.drewjya.crud_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrudApiApplication>(*args)
+}
